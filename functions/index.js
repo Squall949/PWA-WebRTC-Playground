cdabc94da0cff7361ab2d28b7dbe031ceda89725
Webber Lin <webber.lin@thinkpower-info.com>
2018-09-13 03:08:00 +1000

    Adding Firebase Functions for Push

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const webpush = require('web-push');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+
+exports.doPush = functions.https.onCall((data, context) => {
+    admin.database().ref('subscriptions').once('value').then(function(subs) {
+        subs.forEach(function(sub) {
+            webpush.setVapidDetails(
+                'mailto:webber949@gmail.com',
+                'BKeua3D7TfNiWARTtyDHfFLBp9mBLI-qHSrvfL2myus5xnj9Jv9ujTs1fFjxx8qRbbWtgUwcJVyc7BdYEnCR_vI',
+                '73Ol_9aNCgC_T1jV1B2-UkXUos5xVjYsJs_8QErguQs'
+            );
+
+            var pushConfig = {
+                endpoint: sub.val().endpoint,
+                keys: {
+                    auth: sub.val().keys.auth,
+                    p256dh: sub.val().keys.p256dh
+                }
+            };
+            webpush.sendNotification(pushConfig, 
+                JSON.stringify({title: 'Watch my offline stream', content: 'enjoy it!'})
+            )
+            .catch(function(err) {
+                console.log('Creating Push failed', err);
+            });
+        });
+    });
+});
