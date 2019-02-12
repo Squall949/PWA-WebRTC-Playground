f1498da08e36b33326da1059e884238702e328d7
Webber Lin <webber.lin@thinkpower-info.com>
2019-02-12 18:41:37 +0800

    test

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
 
     this.setState({localStream: stream});
     this.localVideo.current.srcObject = stream;
+    this.remoteVideo.current.srcObject = stream;
 
     this.initPeerConnection();
   }
@@ -154,8 +155,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
         <div className="App-videos">
-          <video id="localVideo" autoPlay ref={this.localVideo}></video>
-          <video id="remoteVideo" autoPlay ref={this.remoteVideo}></video>
+          <video id="localVideo" autoPlay playsInline ref={this.localVideo}></video>
+          <video id="remoteVideo" autoPlay playsInline ref={this.remoteVideo}></video>
         </div>
 
         <div>
